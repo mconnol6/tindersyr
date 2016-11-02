f8ed88bed0e412a855628070903cf1160bad9315
mconnol6 <mconnol6@nd.edu>
2016-11-02 01:33:40 -0400

    user can edit their info

--- tindersyr.py
+++ tindersyr.py
@@ -89,10 +89,67 @@ class signup(Resource):
             cursor.callproc('AddUser', (n_netid, n_name, n_year, n_bio, n_hometown, n_gender, n_interested_in, n_number, 0))
             data = cursor.fetchall()
 
-            print data
+            conn.commit()
+            return redirect(url_for('index'))
+
+        except Exception as e:
+            return {'error' : str(e)}
+
+class edit_user(Resource):
+    def get(self):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('GetUser', (session['username'],))
+            data = cursor.fetchall()
+
+            user = data[0]
+            
+            name = user[1]
+            year = user[2]
+            bio = user[3]
+            hometown = user[4]
+            gender = user[5]
+            interested_in = user[6]
+            number = user[7]
 
             conn.commit()
-            #return {'StatusCode' : '200', 'Message' : 'Success'}
+            
+            headers = {'Content-Type': 'text/html'}
+            return make_response(render_template('edit_user.html', netid=session['username'], name=name, year=year, bio=bio, hometown=hometown, gender=gender, interested_in=interested_in, number=number),200,headers)
+
+        except Exception as e:
+            return {'error' : str(e)}
+
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('name', type=str)
+            parser.add_argument('year', type=int)
+            parser.add_argument('bio', type=str)
+            parser.add_argument('hometown', type=str)
+            parser.add_argument('gender', type=str)
+            parser.add_argument('interested_in', type=str)
+            parser.add_argument('number', type=str)
+            args = parser.parse_args()
+
+            n_netid = session['username']
+            n_name = args['name']
+            n_year = args['year']
+            n_bio = args['bio']
+            n_hometown = args['hometown']
+            n_gender = args['gender']
+            n_interested_in = args['interested_in']
+            n_number = args['number']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('UpdateUser', (n_netid, n_name, n_year, n_bio, n_hometown, n_gender, n_interested_in, n_number,))
+            data = cursor.fetchall()
+
+            conn.commit()
             return redirect(url_for('index'))
 
         except Exception as e:
@@ -100,8 +157,10 @@ class signup(Resource):
 
 class index(Resource):
     def get(self):
+        if 'username' not in session:
+            return redirect(url_for('login'))
         headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('hello.html', name=session['username']),200,headers)
+        return make_response(render_template('index.html', name=session['username']),200,headers)
 
 class login(Resource):
     def get(self):
@@ -133,15 +192,20 @@ class login(Resource):
             return {'error': str(e)}
 
 class logout(Resource):
-    def get(self):
-        session.pop('username', None)
+    def post(self):
+        if 'username' in session:
+            session.pop('username', None)
         return redirect(url_for('login'))
 
+@app.route('/')
+def goto_index():
+    return redirect(url_for('index'))
 
 api.add_resource(signup, '/signup')
 api.add_resource(login, '/login')
 api.add_resource(index, '/index')
 api.add_resource(logout, '/logout')
+api.add_resource(edit_user, '/edit_user')
 #api.add_resource(AddOrg, '/AddOrg')
 
 if __name__ == '__main__':
